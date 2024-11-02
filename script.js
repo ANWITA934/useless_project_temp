bf8045d53c56757f405db0551eaa25c64d5b3dfc
ANWITA934 <akm93758@gmail.com>
2024-11-02 23:38:32 +0530

    script.js

--- script.js
+++ script.js
@@ -3,51 +3,51 @@
 const quotes = [
     {
         content: "“They say laughter is the best medicine. That’s why I laugh at funerals.",
-        
+        author: "Rani Rasam"
     },
     {
         content: "I’d agree with you, but then we’d both be wrong.",
-        author: "Anonymous"
+        author: "Chacko Chalu"
     },
     {
         content: "I told my doctor I broke my arm in two places. He told me to stop going to those places.",
-        author: "Unknown"
+        author: "Kochu Kannan"
     },
     {
         content: "People say nothing is impossible, but I do nothing every day.",
-        author: "Unknown"
+        author: "Mallu Manikandan"
     },
     {
         content: "Some people just need a high-five… in the face… with a chair.",
-        author: "Unknown"
+        author: "Shyam Sundar"
     },
     {
         content: "I’d like to help you out. Which way did you come in?",
-        author: "Anonymous"
+        author: "Narayana Nonsense"
     },
     {
         content: "I used to have an open mind, but my brains kept falling out.",
-        author: "Unknown"
+        author: "Lalettan Laughs"
     },
     {
         content: "“Why don’t graveyards have Wi-Fi? Because people are just dying to get in.",
-        author: "Unknown"
+        author: "Golu Gappa"
     },
     {
         content: "I don’t need a hairdresser; my pillow gives me a new hairstyle every morning.",
-        author: "Unknown"
+        author: "Munni Badnaam"
     },
     {
         content: "They say laughter is the best medicine. That’s why I laugh at funerals.",
-        author: "Unknown"
+        author: "Dholak Dada"
     },
     {
         content: "“I’m reading a book on anti-gravity. It’s impossible to put down!",
-        author: "Unknown"
+        author: "Mallu Mutton"
     },
     {
         content: "“I have a fear of elevators, but I’m taking steps to avoid it.",
-        author: "Unknown"
+        author: "Babu Biryani"
     }
 
 ];
